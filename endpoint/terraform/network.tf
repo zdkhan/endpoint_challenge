dbdbf670daf1512c569300dba24bf46804e509e3
Zaid Khan <zdkhan@ucdavis.edu>
2022-03-28 20:23:39 -0700

    fix:resource name typo

--- endpoint/terraform/network.tf
+++ endpoint/terraform/network.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "dev-public-crt" {
     }
 }
 
-resource "aws_route_table_association" "prod-crta-public-subnet-1"{
+resource "aws_route_table_association" "dev-crta-public-subnet-1"{
     subnet_id = "${aws_subnet.dev-subnet-public-1.id}"
     route_table_id = "${aws_route_table.dev-public-crt.id}"
 }
